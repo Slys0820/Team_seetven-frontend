217686774db8e23592ee48348af521b4efc7724b
Slys0820 <ltr0820@gmail.com>
2026-06-29 19:19:05 +0900

    기본설정 찐막

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import styled from "styled-components";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { GlobalStyles } from "./styles";
 import MainPage from "./pages/MainPage";
 
 
 function App() {
   return (
+    <>
+    <GlobalStyles/>
           <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
         </Routes>
       </BrowserRouter>
+      </>
   );
 }
 
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: #fff8f3;
+    border-radius: 12px;
+    color:  #4c3f3c;
+  }
+`;
+
+export default GlobalStyles;
