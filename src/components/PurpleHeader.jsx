857397dd17f5b1fd26ecf25cd4a5679ed70289f3
cy123-coder <wolgugu@gmail.com>
2026-07-05 21:41:16 +0900

    Fix:PurpleHeader 스타일 수정 + 프로필 작성 페이지 틀(미완)

--- src/components/PurpleHeader.jsx
+++ src/components/PurpleHeader.jsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const HeaderStyle = styled.div`
   top: 0;
   width: 100%;
-  height: 50px;
+  height: 10vh;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -16,34 +16,55 @@ const HeaderStyle = styled.div`
   position: relative;
 
   h1 {
-    font-size: 1.3rem;
-    font-weight: normal;
+    font-size: 1.05rem;
+    font-weight: 550;
     margin: 0;
     color: #ffffff;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%); /* 완전 가운데! */
   }
 `;
 
-export default function PurpleHeader({ title, type }) {
+const SetRayout = styled.div`
+  // 뒤로가기와 타이틀을 묶어서 한번에 배치하는 틀
+  display: flex;
+  top: 60%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+`;
+
+const BackButton = styled.div`
+  // 뒤로 가기 버튼 자체 제작
+  cursor: pointer;
+  margin-right: auto; /* 왼쪽 끝으로! */
+  width: 13px;
+  height: 13px;
+  aspect-ratio: 1/1;
+  border-left: 3px solid white;
+  border-bottom: 3px solid white;
+  border-radius: 1px; /* 살짝 둥글게! */
+  transform: rotate(45deg);
+  cursor: pointer;
+  margin-left: 5%;
+  flex-shrink: 0; /* 비율 안 깨지게! */
+`;
+
+export default function PurpleHeader({ title }) {
   const navigate = useNavigate();
   return (
     <HeaderStyle>
       {/* 왼쪽 뒤로가기 버튼 */}
-      <span
-        onClick={() => navigate(-1)} // 이전 페이지로 이동
-        style={{
-          cursor: "pointer",
-          position: "absolute",
-          left: "16px",
-          fontSize: "1.2rem",
-          fontWeight: "bold",
-          color: "#ffffff",
-        }}
-      >
-        &lt; {/* 부등호 기호 출력 */}
-      </span>
+      <SetRayout>
+        <BackButton
+          onClick={() => navigate(-1)} // 이전 페이지로 이동
+        />
 
-      {/* 중앙 타이틀 */}
-      <h1>{title}</h1>
+        {/* 중앙 타이틀 */}
+        <h1>{title}</h1>
+      </SetRayout>
     </HeaderStyle>
   );
 }
