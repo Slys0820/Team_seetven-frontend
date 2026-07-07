6b25f0fc1c8daeee28bc84731f84beafb263ed1b
cy123-coder <wolgugu@gmail.com>
2026-07-07 15:08:23 +0900

    7번 페이지 틀

---
+++ src/pages/WriteGather.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+function WriteGather() {
+  return (
+    <>
+      {" "}
+      <> 아니아ㅣㅣ닞덝절기</>
+    </>
+  );
+}
+
+export default WriteGather;
