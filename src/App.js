b824529ce33558eb900b95a1891ad54cbda39bb7
Slys0820 <ltr0820@gmail.com>
2026-07-09 03:30:29 +0900

    Feat My팀 페이지 완성

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import WholePost from "./pages/WholePost";
 import Storage from "./pages/Storage";
 import AcceptEnd from "./pages/AcceptEnd";
 import WriteGather from "./pages/WriteGather";
+import MyTeam from "./pages/MyTeam";
 
 // 💡 1. 주소를 감시하고 레이아웃을 뿌려줄 실질적인 메인 컴포넌트
 function AppContent() {
@@ -43,6 +44,7 @@ function AppContent() {
         <Route path="/writeend" element={<WriteEnd />} />
         <Route path="/acceptend" element={<AcceptEnd />} />
         <Route path="/makeprofilecard" element={<MakeProfileCard />} />
+        <Route path="/myteam" element={<MyTeam />} />
       </Routes>
 
       {/* 💡 네비게이션 바가 존재하는 창에서는 네비게이션을 보여주기*/}
